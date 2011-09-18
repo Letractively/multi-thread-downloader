5f3e12e33c17b1fcd44dcbdc21fd33ab9ec6b6d0
jjurco.sk@gmail.com <jjurco.sk@gmail.com@5961207c-5997-db96-4fe2-1cc73c40195a>
2011-09-18 09:34:17 +0000

    create new project

---
+++ multi-thread-downloader/src/main/java/sk/lieskove/jianghongtiao/multithreaddownloader/App.java
@@ -0,0 +1,13 @@
+package sk.lieskove.jianghongtiao.multithreaddownloader;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
